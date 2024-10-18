20f5a605a4a669552c711c317d8b8d233e044573
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-10-18 20:24:28 +0800

    ci: fix ci test before route build

--- packages/backend/scripts/build.ts
+++ packages/backend/scripts/build.ts
@@ -8,5 +8,8 @@ const __dirname = getCurrentPath(import.meta.url);
 
 execSync('tsc -p tsconfig.build.json && tsc-alias')
 fs.mkdirSync(path.join(__dirname, '../dist/assets/build'), { recursive: true });
+fs.mkdirSync(path.join(__dirname, '../assets/build'), { recursive: true });
 fs.writeFileSync(path.join(__dirname, '../dist/assets/build/routes.json'), JSON.stringify(namespace.namespaces, null, 2));
+fs.writeFileSync(path.join(__dirname, '../assets/build/routes.json'), JSON.stringify(namespace.namespaces, null, 2));
 fs.writeFileSync(path.join(__dirname, '../dist/assets/build/credential-schemas.json'), JSON.stringify(namespace.credentials, null, 2));
+fs.writeFileSync(path.join(__dirname, '../assets/build/credential-schemas.json'), JSON.stringify(namespace.credentials, null, 2));
